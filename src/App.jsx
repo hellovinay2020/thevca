85de4d5cfeab21c1bcb9f36d9851b1943249d57c
Vinay Kumar Verma <vinaycomputer08@gmail.com>
2026-06-24 17:51:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-﻿import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
 import OurMission from './Pages/OurMission'
 import OurVision from './Pages/OurVision'
 import Director from './Pages/Director'
